d757346770adaefeb9b48844f807403ff9aa196e
Martin Braun <martinbraun123@aol.com>
2018-05-22 10:33:24 +0200

    Add files via upload

--- conversion.c
+++ conversion.c
@@ -9,12 +9,25 @@ _float_t conversion_voltage_to_ppm(_float_t voltage) {
 }
 
 _float_t conversion_voltage_to_pct(_float_t voltage) {
+	_float_t result = 0;
+	if(voltage < 350) {
+		_float_t m = 0.11525f;
+		_float_t C = -35.40378f;
+		result = m * voltage + C;
+	} else if (voltage < 668) {
+		_float_t a = 0.52556f;
+		_float_t b = 0.00630f;
+		result = a * exp(b * voltage);
+	} else {
+		result = 100;
+	}
+	return result;
 	// Gerade:_float_t pct = (_float_t) ((((_double_t) voltage) - 313.533) / 8.03);
 	//
-	_float_t A = 0.362f;
-	_float_t B = 0.79f;
-	_float_t C = 343.7f;
-	_float_t eps = 15.7f;
-	_float_t pct = (_float_t) ((-B + sqrt(A*A - (B*B) * (C - (_double_t) voltage) + eps)) / 2*A);
-	return MAX(pct, 0.0f);
+	//_float_t A = 0.362f;
+	//_float_t B = 0.79f;
+	//_float_t C = 343.7f;
+	//_float_t eps = 15.7f;
+	//_float_t pct = (_float_t) ((-B + sqrt(A*A - (B*B) * (C - (_double_t) voltage) + eps)) / 2*A);
+	//return MAX(pct, 0.0f);
 }
