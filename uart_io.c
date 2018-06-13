d3ec15469d46dfd993f8e22bf5dfb80ecf3235a5
Martin Braun <martinbraun123@aol.com>
2018-06-13 09:16:08 +0200

    Update uart_io.c

--- uart_io.c
+++ uart_io.c
@@ -13,24 +13,15 @@
 #include "stdtypes.h"
 
 void uart_io_init() {
-	//
-	// Enable the peripherals used by this example.
-	//
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 	ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
 
 	ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-	//
-	// Configure the UART for 115,200, 8-N-1 operation.
-	//
 	ROM_UARTConfigSetExpClk(UART0_BASE, ROM_SysCtlClockGet(), 9600,
 	                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
 	                         UART_CONFIG_PAR_NONE));
 
-	//
-	// Enable the UART interrupt.
-	//
 	ROM_IntEnable(INT_UART0);
 	ROM_UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
 }
